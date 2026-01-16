6f0b6162d4e107fd9657a93f9152493a7a4fe618
Ngan Jin Huey <JHngan@Jin-MacBook-Air.local>
2026-01-16 12:31:28 +0800

    Remove multi-language support and fix syntax

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import HomeBM from './pages/HomeBM';
-import HomeCN from './pages/HomeCN';
+
 import HomeV2 from './pages/HomeV2';
 
 const App: React.FC = () => {
@@ -10,8 +9,7 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<HomeV2 />} />
-        <Route path="/bm" element={<HomeBM />} />
-        <Route path="/cn" element={<HomeCN />} />
+
         <Route path="/v2" element={<HomeV2 />} />
       </Routes>
     </Router>
